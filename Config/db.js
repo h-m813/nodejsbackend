ce19d0143755424840d5a87080b7599e50cb2ace
Hem Singh <hemsinghs434@gmail.com>
2025-07-08 12:45:47 +0530

    Fix: Use correct Railway DB config

--- Config/db.js
+++ Config/db.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost", // or "127.0.0.1"
-  port: 3306, // default MySQL port
-  user: "root", // default username in local setup
-  password: "admin", // default password is empty in XAMPP/WAMP
-  database: "myprojectdb", // change to your local database name
+  host: "shuttle.proxy.rlwy.net",
+  port: 11652,
+  user: "root",
+  password: "DzdomjpyNWMxfGMKVASSBvzKJLVXtwbg",
+  database: "railway",
 });
 
 db.connect((err) => {
   if (err) {
     console.error("❌ DB connection failed:", err);
   } else {
-    console.log("✅ Connected to Local MySQL");
+    console.log("✅ Connected to Railway MySQL");
   }
 });
 
